89c8248b88103e15556a6ac81323e3dc5add04f0
labstraction <labstraction@protonmail.com>
2025-10-22 09:59:44 +0200

    esercizi 1

--- esercizi.js
+++ esercizi.js
@@ -3,3 +3,54 @@
 //3) chiedete all'utente un numero e ditegli se è pari o dispari (operatore modulo)
 //4) fate inserire all'utente l'età e se è maggiore o uguale a 18 rispondete "puoi entrare"
 //5) fate inserire all'utente una stringa e convertitela in maiuscolo (cercare su internet)
+
+// 1) ////////////////////////////////////////
+// const firstName = prompt("Come ti chiami?");
+// alert("Tanti Auguri a "+firstName);
+
+
+// 2) ////////////////////////////////////////
+// const numberOne = prompt("Dammi la base della potenza");
+// const numberTwo = prompt("Dammi l'esponente");
+
+// const n1Converted = Number(numberOne);
+// const n2Converted = Number(numberTwo);
+
+// const result = n1Converted**n2Converted;
+
+// alert("Il risultato è: "+result)
+
+// 3) /////////////////////////////////////////
+
+// const numb = prompt("dammi un numero");
+
+// const numbConverted = Number(numb);
+
+// const isEven = numbConverted % 2 === 0;
+
+// if (isEven) {
+//     alert("pari")
+// } else {
+//     alert("dispari")
+// }
+
+
+// 4) /////////////////////////////////////////
+
+// const age = prompt("dammi l'eta")
+
+// const ageConverted = Number(age);
+
+// const isAdult = ageConverted >= 18;
+
+// if (isAdult) {
+//     alert("puoi entrare")
+// }
+
+// 5) /////////////////////////////////////////
+
+// let word= prompt("insert stirngs of words");
+// let wordUperCase = word.toUpperCase();
+// // let wordUperCase = word.toLocaleUpperCase;
+
+// alert(wordUperCase);
